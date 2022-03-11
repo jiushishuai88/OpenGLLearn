54e81c64236d38a696d5a33d0c143d733f554638
春之 <644930690@qq.com>
2022-03-11 18:21:05 +0800

    +add shader class

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -2,23 +2,8 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "Shader.h"
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-const char* fragmentShaderSource =
-"#version 330 core\n"
-" out vec4 FragColor;\n"
-" void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-" }";
-
-unsigned int shaderProgram;
 unsigned int VAO;
 unsigned int VBO;
 unsigned int EBO;
@@ -32,10 +17,10 @@ void processInput(GLFWwindow* window)
 void BindData()
 {
     float vertices[] = {
-     0.5f, 0.5f, 0.0f,   // 右上角
-     0.5f, -0.5f, 0.0f,  // 右下角
-     -0.5f, -0.5f, 0.0f, // 左下角
-     -0.5f, 0.5f, 0.0f   // 左上角
+     0.5f, 0.5f, 0.0f,    1.0f,0.0f,0.0f, // 右上角
+     0.5f, -0.5f, 0.0f,   0.0f,1.0f,0.0f,// 右下角
+     -0.5f, -0.5f, 0.0f,  0.0f,0.0f,1.0f,// 左下角
+     -0.5f, 0.5f, 0.0f,   1.0f,1.0f,0.0f   // 左上角
     };
 
     unsigned int indices[] = { // 注意索引从0开始! 
@@ -54,41 +39,15 @@ void BindData()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
     //解绑
     glBindBuffer(GL_ARRAY_BUFFER,0);
     glBindVertexArray(0);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-}
-
-void BuildShader()
-{
-    //顶点着色器
-
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    //片段着色器
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    int success;
-    char infoLog[2];
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteProgram(vertexShader);
-    glDeleteProgram(fragmentShader);
 }
 
 int main()
@@ -123,7 +82,8 @@ int main()
             glViewport(0, 0, width, height);
         });
     //编译着色器
-    BuildShader();
+    Shader shader = { "D:\\workSpace\\CodeSpace\\OpenGLLearn\\helloworld\\shaders\\hellowrold.vert","D:\\workSpace\\CodeSpace\\OpenGLLearn\\helloworld\\shaders\\helloworld.frag" };
+    shader.use();
     BindData();
 
     while (!glfwWindowShouldClose(window))
@@ -132,9 +92,9 @@ int main()
         //渲染
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
 
         glfwSwapBuffers(window);
@@ -144,7 +104,6 @@ int main()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
     glfwTerminate();
     return 0;
 }
